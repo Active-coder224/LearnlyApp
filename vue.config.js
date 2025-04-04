1e9eb13198b4b99e184b23e154ccf21756d3d9a7
Adeola <adesanmiadeola18@gmail.com>
2025-04-04 12:46:30 +0100

    error in logs

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
-  devServer: {
-    port: 8080,
-    open: true,
-    historyApiFallback: true,
-  },
-  configureWebpack: {
-    performance: {
-      hints: false,
-    },
-  },
+  publicPath: "/",
 };
